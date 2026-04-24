4fc64f8032869228b682d5b32786bf1e02cfa6ff
mateo2002xd1 <mateo2002xd1@gmail.com>
2026-04-24 17:44:09 -0500

    Se resulve problema con FastAPI

--- routers/productos_endpoints.py
+++ routers/productos_endpoints.py
@@ -1,26 +1,5 @@
 from fastapi import APIRouter
 from services.productos_service import (
-<<<<<<< HEAD
-    producto,
-    post_ingresar_producto,
-    get_mostrar_productos,
-    get_mostrar_producto_codigo
-)
-
-router = APIRouter(prefix="/productos")
-
-@router.post("/ingresar")
-def post_ingresar_producto_endpoint(producto_test: producto):
-        return post_ingresar_producto(producto_test)
-
-@router.get("/mostrar_all")
-def get_mostrar_productos_endpoint():
-        return get_mostrar_productos()
-
-@router.get("/mostrar_code/{codigo}")
-def get_mostrar_producto_codigo_endpoint(codigo: int):
-    return get_mostrar_producto_codigo(codigo)
-=======
     ingresar_producto,
     mostrar_productos,
     mostrar_producto_codigo,
@@ -50,4 +29,3 @@ def reemplazar_producto_endpoint(codigo: int, producto_body: producto):
 @router.delete("/{codigo}")
 def eliminar_producto_endpoint(codigo: int):
       return eliminar_producto(codigo)
->>>>>>> c56ca4bb137baba9beda49c920459f7606142854
--- routers/usuarios_endpoints.py
+++ routers/usuarios_endpoints.py
@@ -1,13 +1,4 @@
 from fastapi import APIRouter
-<<<<<<< HEAD
-from services.usuarios_service import usuario, post_ingreso_usuario
-
-router = APIRouter(prefix="/usuarios")
-
-@router.post("/ingresar")
-def post_ingreso_usuario_endpoint(usuario_ingresar: usuario):
-    return post_ingreso_usuario(usuario_ingresar)
-=======
 from services.usuarios_service import ingreso_usuario
 from models.productos import usuario
 
@@ -16,4 +7,3 @@ router = APIRouter(prefix="/usuarios")
 @router.post("/")
 def ingreso_usuario_endpoint(usuario_ingresar: usuario):
     return ingreso_usuario(usuario_ingresar)
->>>>>>> c56ca4bb137baba9beda49c920459f7606142854
