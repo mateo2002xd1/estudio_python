613150d0d5e615a50e93c207d26574d9922d3511
mateo2002xd1 <mateo2002xd1@gmail.com>
2026-05-05 15:24:24 -0500

    Configuracion de docker

--- app/database.py
+++ app/database.py
@@ -1,12 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import time
 
-DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/fastapi_db"
+DATABASE_URL = "postgresql+psycopg2://postgres:1234@db:5432/fastapi_db"
 
-engine = create_engine(
-    DATABASE_URL,
-    echo=False
-)
+# 🔥 Retry para esperar a PostgreSQL
+for i in range(10):
+    try:
+        engine = create_engine(
+            DATABASE_URL,
+            echo=False
+        )
+        # intenta conectar
+        connection = engine.connect()
+        connection.close()
+        print("✅ Conectado a PostgreSQL")
+        break
+    except Exception as e:
+        print(f"⏳ Esperando DB... intento {i+1}")
+        time.sleep(2)
+else:
+    raise Exception("❌ No se pudo conectar a la base de datos")
 
 SessionLocal = sessionmaker(
     autocommit=False,
