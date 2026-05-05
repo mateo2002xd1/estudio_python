bb1c4037a4397079dc4bf099095be8f31b087b36
mateo2002xd1 <mateo2002xd1@gmail.com>
2026-05-05 17:00:08 -0500

    Configuracion de render

--- app/database.py
+++ app/database.py
@@ -1,26 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import time
 
-DATABASE_URL = "postgresql+psycopg2://postgres:1234@db:5432/fastapi_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 🔥 Retry para esperar a PostgreSQL
-for i in range(10):
-    try:
-        engine = create_engine(
-            DATABASE_URL,
-            echo=False
-        )
-        # intenta conectar
-        connection = engine.connect()
-        connection.close()
-        print("✅ Conectado a PostgreSQL")
-        break
-    except Exception as e:
-        print(f"⏳ Esperando DB... intento {i+1}")
-        time.sleep(2)
-else:
-    raise Exception("❌ No se pudo conectar a la base de datos")
+if not DATABASE_URL:
+    raise Exception("❌ DATABASE_URL no está definida")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
--- app/main.py
+++ app/main.py
@@ -3,14 +3,30 @@ from app.routers.productos_endpoints import router as productos_router
 from app.routers.auth_endpoints import router as auth_router
 from app.routers.usuarios_endpoints import router as usuarios_router
 from app.database import Base, engine
-from app.models.productos_db import Producto
-from app.models.usuarios_db import Usuario
+from sqlalchemy import text
+import time
 
 app = FastAPI()
 
+def init_db():
+    for i in range(10):
+        try:
+            with engine.connect() as conn:
+                conn.execute(text("SELECT 1"))
+
+            Base.metadata.create_all(bind=engine)
+            print("✅ DB lista y tablas creadas")
+            return
+
+        except Exception as e:
+            print(f"⏳ Esperando DB... intento {i+1}")
+            time.sleep(2)
+
+    raise Exception("❌ No se pudo conectar a la DB")
+
 @app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
+def startup_event():
+    init_db()
 
 app.include_router(productos_router)
 app.include_router(usuarios_router)
